63bfed1acbefca14201976d3d3be6900c470690a
João Peixoto - Work <joaottvitor04@gmail.com>
2025-11-24 22:20:45 -0300

    feat: Configurando eureka

---
+++ service-discovery-eureka/src/test/java/com/POG/service_discovery_eureka/ServiceDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POG.service_discovery_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
